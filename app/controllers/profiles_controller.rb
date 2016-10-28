d53673fcbf98ede00f2164e2f84fc24a09e7c790
Rick Sanchez <rick0112@gmail.com>
2016-10-28 13:35:09 +0000

    Added create profile link in home page

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,12 +1,16 @@
 class ProfilesController < ApplicationController
-  
+
   def new
+    # new user profile
+    @user = User.find( params[:user_id] ) # find logged in user
+    @profile = @user.build_profile
+    # Build Profile is a special function that we can use because of db associations and routes:
   end
-  
+
   def create
   end
-  
+
   def edit
   end
-  
+
 end
