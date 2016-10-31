8646e199d6aeb64bd65d4858c54314d80080462f
Rick Sanchez <rick0112@gmail.com>
2016-10-31 10:24:49 +0000

    Added edit controller, form partials

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -19,11 +19,13 @@ class ProfilesController < ApplicationController
       # redirect user to the new controller
     end
   end
-  
+
   def show
   end
 
   def edit
+    @user = User.find(params[:user_id])
+    @profile = @user.profile
   end
 
   private
